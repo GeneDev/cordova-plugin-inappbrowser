568d02e04aa396f4b4df0ab4765f02b2742937ed
EXphinx <EXphinxDev@gmail.com>
2014-03-02 19:00:08 +0800

    edits

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -468,8 +468,8 @@ public class InAppBrowser extends CordovaPlugin {
                 toolbar.setBackgroundColor(android.graphics.Color.LTGRAY);
                 toolbar.setLayoutParams(new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT, this.dpToPixels(44)));
                 toolbar.setPadding(this.dpToPixels(2), this.dpToPixels(2), this.dpToPixels(2), this.dpToPixels(2));
-                toolbar.setHorizontalGravity(Gravity.LEFT);
                 toolbar.setVerticalGravity(Gravity.BOTTOM);
+                toolbar.setHorizontalGravity(Gravity.LEFT);
 
                 // Action Button Container layout
                 RelativeLayout actionButtonContainer = new RelativeLayout(cordova.getActivity());
@@ -508,7 +508,7 @@ public class InAppBrowser extends CordovaPlugin {
                 // Forward button
                 Button forward = new Button(cordova.getActivity());
                 RelativeLayout.LayoutParams forwardLayoutParams = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT);
-                forwardLayoutParams.addRule(RelativeLayout.RIGHT_OF, 12);
+                forwardLayoutParams.addRule(RelativeLayout.RIGHT_OF, 20);
                 forward.setLayoutParams(forwardLayoutParams);
                 forward.setContentDescription("Forward Button");
                 forward.setId(3);
