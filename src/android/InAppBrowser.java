a3e137582303f5f1a4282af22b961e4d0bb06531
EXphinx <EXphinxDev@gmail.com>
2014-03-02 18:35:34 +0800

    move toolbar to bottom.

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -469,7 +469,7 @@ public class InAppBrowser extends CordovaPlugin {
                 toolbar.setLayoutParams(new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT, this.dpToPixels(44)));
                 toolbar.setPadding(this.dpToPixels(2), this.dpToPixels(2), this.dpToPixels(2), this.dpToPixels(2));
                 toolbar.setHorizontalGravity(Gravity.LEFT);
-                toolbar.setVerticalGravity(Gravity.TOP);
+                toolbar.setVerticalGravity(Gravity.BOTTOM);
 
                 // Action Button Container layout
                 RelativeLayout actionButtonContainer = new RelativeLayout(cordova.getActivity());
